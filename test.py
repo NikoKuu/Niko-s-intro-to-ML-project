0feac0a9c4fd6728965a97e15b115e315a526a2a
NikoKuu <niko.kuusniemi@gmail.com>
2024-09-16 19:46:02 -0700

    Update

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello world')
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+myocardial_infarction_complications = fetch_ucirepo(id=579) 
+  
+# data (as pandas dataframes) 
+X = myocardial_infarction_complications.data.features 
+y = myocardial_infarction_complications.data.targets 
+  
+# metadata 
+print(myocardial_infarction_complications.metadata) 
+  
+# variable information 
+print(myocardial_infarction_complications.variables) 
+print(type(myocardial_infarction_complications))
